d7b81528c3794dd6c19713c7b447026749ee2a7e
unknown <kennytruong29@yahoo.com>
2026-07-03 11:19:51 -0700

    refactor conditional statements and add logic to extract content

--- getResume.py
+++ getResume.py
@@ -1,6 +1,9 @@
 from typing import Annotated
 from fastapi import FastAPI, File, Form, UploadFile, HTTPException
 from pathlib import Path
+import pdfplumber
+from io import BytesIO
+from docx import Document
 
 app = FastAPI()
 
@@ -9,6 +12,23 @@ acceptedExtensions = ['.pdf','.docx']
 @app.post("/files/")
 async def getResume(file: UploadFile, jobDescription: Annotated[str, Form()]):
     fileExtension = Path(file.filename).suffix.lower()
-    if not any(x in fileExtension for x in acceptedExtensions):
+
+    if fileExtension not in acceptedExtensions:
         raise HTTPException(status_code=400, detail="Incorrect file type uploaded")
-    content = await file.read()
+    
+    fileBytes = await file.read()
+    content = BytesIO(fileBytes)
+
+    if fileExtension == '.pdf':
+        return pullContentPDF(content)   
+    else:
+        return pullContentDoc(content)
+    
+def pullContentPDF(file: BytesIO):
+    with pdfplumber.open(file) as pdf:
+        return pdf.pages[0].extract_text(x_tolerance=1, y_tolerance=1)
+    
+def pullContentDoc(file: BytesIO):
+    doc = Document(file)
+    doc_content = [para.text for para in doc.paragraphs]
+    return doc_content
